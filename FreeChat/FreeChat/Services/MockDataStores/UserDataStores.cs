5ad78c772c5a3db0699803aafcae88ece85e1af6
Lcshenrique1 <lcshenrique@live.com>
2022-07-11 21:25:27 -0300

    Atualizando as telas

--- FreeChat/FreeChat/Services/MockDataStores/UserDataStores.cs
+++ FreeChat/FreeChat/Services/MockDataStores/UserDataStores.cs
@@ -18,17 +18,17 @@ namespace FreeChat.Services.MockDataStores
             _users.Add(new User("Alfredo", "Stephano", "I like chatting online and making new friends on social media",
                 "alfredo.jpg", 5, 230)
             { Id = Guid.NewGuid().ToString(), IsOnline = true });
-            _users.Add(new User("Carelle", "du Pont", "Chatting is sooooo fun and, you can come and talk to me if you are bored or in need of some random conversation."
-                , "carelle.jpg", 10, 1500)
-            { Id = Guid.NewGuid().ToString() });
-            _users.Add(new User("Rebeca", "Wong", "I'm funny, I like playing baket ball and watching movies. I like talking here too. Lets have a talk",
-                "rebeca.jpg", 15, 100)
-            { Id = Guid.NewGuid().ToString() });
-            _users.Add(new User("John", "Wong", "I'm funny, I like playing baket ball and watching movies. I like talking here too. Lets have a talk",
-                "john.jpg", 2, 10)
-            { Id = Guid.NewGuid().ToString() });
-            _users.Add(new User("Rea", "Mogolar", "Lets chat and have fun !!!", "rea.jpg", 5, 230)
-            { Id = Guid.NewGuid().ToString() });
+        //    _users.Add(new User("Carelle", "du Pont", "Chatting is sooooo fun and, you can come and talk to me if you are bored or in need of some random conversation."
+        //        , "carelle.jpg", 10, 1500)
+        //    { Id = Guid.NewGuid().ToString() });
+        //    _users.Add(new User("Rebeca", "Wong", "I'm funny, I like playing baket ball and watching movies. I like talking here too. Lets have a talk",
+        //        "rebeca.jpg", 15, 100)
+         //   { Id = Guid.NewGuid().ToString() });
+        //    _users.Add(new User("John", "Wong", "I'm funny, I like playing baket ball and watching movies. I like talking here too. Lets have a talk",
+        //        "john.jpg", 2, 10)
+        //    { Id = Guid.NewGuid().ToString() });
+        //    _users.Add(new User("Rea", "Mogolar", "Lets chat and have fun !!!", "rea.jpg", 5, 230)
+        //   { Id = Guid.NewGuid().ToString() });
         }
 
         public Task<bool> AddItemAsync(User item)
